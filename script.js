b9f8608c0e5d0c76bd100dfa008c08764beefee9
ashleydelic <ashleycohzer96@gmail.com>
2022-11-13 16:31:31 -0500

    Update script.js
    added the hasBlackJack boolean for a win

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let firstCard = 2;
-let secondCard = 9;
+let firstCard = 10;
+let secondCard = 11;
+let hasBlackJack = false;
 
 let sum = firstCard + secondCard;
 console.log(sum);
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card?  🙂") 
 } else if (sum === 21) {
     console.log("Wooho! You've got Blackjack! 🥳")
+    hasBlackJack = true;
 } else {
     console.log("You are out of the game! 👎")
 }
