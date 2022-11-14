9e449ccd8f46688907cc179250a059fc27bf75d5
ashleydelic <ashleycohzer96@gmail.com>
2022-11-13 20:36:47 -0500

    added the cards elements on html and js

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let message = "";
 
 let meesageEl = document.getElementById("message-el");
 let sumEL = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el")
 
 
 
 function startGame() {
     sumEL.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
