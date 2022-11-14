cbd44ffec9f3badacb32681ec448a49062bfd704
ashleydelic <ashleycohzer96@gmail.com>
2022-11-13 21:14:59 -0500

    converted the cards into an array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 4;
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isActive = true;
@@ -16,7 +17,7 @@ function startGame() {
 
 function renderGame() {
     sumEL.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
